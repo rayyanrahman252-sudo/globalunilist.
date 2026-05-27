77f137ca648bbe3f5a65c08159168d77f4c47b00
rayyanrahman252-sudo <rayyanrahman252@gmail.com>
2026-05-27 16:35:29 +0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
-import "tailwindcss/tailwind.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: any) {
   return <Component {...pageProps} />;
 }
